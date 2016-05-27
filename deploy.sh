e5f2792f87c1c39ea37c8f255450439fad972c6a
southorange <southorange@users.noreply.github.com>
2016-05-26 22:22:42 -0400

    Purple (#2)
    * Create .travis.yml
    
    * Create requirements.txt
    
    * Create deploy.sh
    
    * Update deploy.sh
    
    * Update .travis.yml
    
    * Update .travis.yml (#2)
    
    * Update .travis.yml
    
    * Create py.py
    
    * Update deploy.sh
    
    * Create del.txt
    
    * Delete del.txt
    
    * Update .travis.yml
    
    * Update .travis.yml (#6)
    
    * C
    
    * Delete y
    
    * Create lorem_ipsum.txt
    
    * Create example.txt (#14)
    
    *  _
    
    * \n
    
    * \n
    
    * \"""
    q
    
    * This is a commit.

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+#dependencies
+sudo apt-get update -y
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
+sudo apt-get install --no-install-recommends libboost-all-dev -y
+sudo apt-get install python-dev -y
+sudo apt-get install ffmpeg -y
+
+wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+tar zxvf glog-0.3.3.tar.gz
+cd glog-0.3.3
+./configure
+make && make install
+
+wget https://github.com/schuhschuh/gflags/archive/master.zip
+unzip master.zip
+cd gflags-master
+mkdir build && cd build
+export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
+make && make install
+
+yes | git clone https://github.com/LMDB/lmdb
+cd lmdb/libraries/liblmdb
+make && make install
+
+git clone https://github.com/BVLC/caffe.git
+cd caffe
+cp Makefile.config.example Makefile.config
+make all
+make test
+make runtest
+
+
+#actual usage
+git clone https://github.com/kesara/deepdreamer.git
+cd deepdreamer
+wget https://github.com/BVLC/caffe/blob/master/models/bvlc_googlenet/deploy.prototxt
+echo force_backward: true >> deploy.prototxt
+wget https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet
+python deepdreamer.py
+
+
+
+
